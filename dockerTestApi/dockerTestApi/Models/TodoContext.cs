f5e6f23e5e7bc8e24dc3289ff95a321d0a0df234
Albert Sanchez <albert.san.iza@gmail.com>
2018-07-25 16:49:53 -0700

    add todocontext

---
+++ dockerTestApi/dockerTestApi/Models/TodoContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dockerTestApi.Models {
+    public class TodoContext : DbContext {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) {
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
