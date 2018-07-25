7f113b52ed9b6193df8340da9cd7f7b1e4f91ca1
Albert Sanchez <albert.san.iza@gmail.com>
2018-07-25 16:51:08 -0700

    removed

--- dockerTestApi/dockerTestApi/Models/TodoContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace dockerTestApi.Models {
-    public class TodoContext : DbContext {
-        public TodoContext(DbContextOptions<TodoContext> options) : base(options) {
-        }
-        public DbSet<TodoItem> TodoItems { get; set; }
-    }
-}
