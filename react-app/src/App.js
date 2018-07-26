ec6c03ce25a18af0be6c3f546efd34f34b052fdc
Albert Sanchez <albert.san.iza@gmail.com>
2018-07-26 10:05:29 -0700

    add a list

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,19 +3,27 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            apiList: ["hola", "como"]
+        }
+    }
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <ul className="App-list">
+            {this.state.apiList.map((y, z) => (
+                <li key="{z}">{y}</li>
+            ))}
+            </ul>
+            </div>
+        )
+    }
 }
 
 export default App
