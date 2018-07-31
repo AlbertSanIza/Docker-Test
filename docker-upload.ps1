337d015d69040773033db7ede01d63381119d3d8
Albert Sanchez <albert.san.iza@gmail.com>
2018-07-31 15:55:38 -0700

    remove ssh pass command

--- docker-upload.ps1
+++ docker-upload.ps1
@@ -5,14 +5,14 @@ docker save -o react-app.tar react-app
 docker save -o dockertestapi.tar dockertestapi
 
 "scp"
-sshpass -p $pass scp react-app.tar versacall@192.168.100.231:react-app.tar
-sshpass -p $pass scp dockertestapi.tar versacall@192.168.100.231:dockertestapi.tar
-sshpass -p $pass scp docker-compose.yml versacall@192.168.100.231:docker-compose.yml
+scp react-app.tar versacall@192.168.100.231:react-app.tar
+scp dockertestapi.tar versacall@192.168.100.231:dockertestapi.tar
+scp docker-compose.yml versacall@192.168.100.231:docker-compose.yml
 
 "rm"
 rm react-app.tar
 rm dockertestapi.tar
 
 "docker load"
-sshpass -p $pass ssh versacall@192.168.100.231 "docker load -i react-app.tar && docker load -i dockertestapi.tar"
+ssh versacall@192.168.100.231 "docker load -i react-app.tar && docker load -i dockertestapi.tar"
 pause
