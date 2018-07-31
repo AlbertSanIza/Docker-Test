fbd7f2e9f6d72de13bc85938aa15d90f4b3b2322
Albert Sanchez <albert.san.iza@gmail.com>
2018-07-31 16:02:55 -0700

    add react route sample

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,50 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import logo from './logo.svg'
 import './App.css'
+
+const Home = () => (
+    <div>
+    <h2>Home</h2>
+    </div>
+)
+const About = () => (
+    <div>
+    <h2>About</h2>
+    </div>
+)
+const Topic = ({ match }) => (
+    <div>
+    <h3>{match.params.topicId}</h3>
+    </div>
+)
+const Topics = ({ match }) => (
+    <div>
+    <h2>Topics</h2>
+    <ul>
+    <li>
+    <Link to={`${match.url}/rendering`}>
+    Rendering with React
+    </Link>
+    </li>
+    <li>
+    <Link to={`${match.url}/components`}>
+    Components
+    </Link>
+    </li>
+    <li>
+    <Link to={`${match.url}/props-v-state`}>
+    Props v. State
+    </Link>
+    </li>
+    </ul>
+    <Route path={`${match.path}/:topicId`} component={Topic}/>
+    <Route exact path={match.path} render={() => (
+        <h3>Please select a topic.</h3>
+    )}/>
+    </div>
+)
 
 class App extends Component {
     constructor(props) {
@@ -18,6 +61,8 @@ class App extends Component {
     }
     render() {
         return (
+            <Router>
+            <div>
             <div className="App">
             <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
@@ -29,6 +74,19 @@ class App extends Component {
             {this.state.apiList.map((y, z) => <li key={z}>{z} - {y}</li>)}
             </ul>
             </div>
+            <div>
+            <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/topics">Topics</Link></li>
+            </ul>
+            <hr/>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/topics" component={Topics}/>
+            </div>
+            </div>
+            </Router>
         )
     }
 }
