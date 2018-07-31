27322ebe703529013783e31816446c8bd9ad0bc7
Albert Sanchez <albert.san.iza@gmail.com>
2018-07-31 15:15:12 -0700

    tweak

--- docker-upload.ps1
+++ docker-upload.ps1
@@ -4,9 +4,10 @@ docker save -o dockertestapi.tar dockertestapi
 "scp"
 scp react-app.tar versacall@192.168.100.231:react-app.tar
 scp dockertestapi.tar versacall@192.168.100.231:dockertestapi.tar
+scp docker-compose.yml versacall@192.168.100.231:docker-compose.yml
 "rm"
 rm react-app.tar
-dockertestapi.tar
+rm dockertestapi.tar
 "docker load"
 ssh versacall@192.168.100.231 "docker load -i react-app.tar && docker load -i dockertestapi.tar"
 pause
