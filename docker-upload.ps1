ef84eab4cad442587193ce8db2d4f4e68b779ef3
Albert Sanchez <albert.san.iza@gmail.com>
2018-07-31 15:43:51 -0700

    uploder works good

--- docker-upload.ps1
+++ docker-upload.ps1
@@ -1,13 +1,18 @@
+$pass = Read-Host "Enter Password" -AsSecureString
+
 "docker save"
 docker save -o react-app.tar react-app
 docker save -o dockertestapi.tar dockertestapi
+
 "scp"
-scp react-app.tar versacall@192.168.100.231:react-app.tar
-scp dockertestapi.tar versacall@192.168.100.231:dockertestapi.tar
-scp docker-compose.yml versacall@192.168.100.231:docker-compose.yml
+sshpass -p $pass scp react-app.tar versacall@192.168.100.231:react-app.tar
+sshpass -p $pass scp dockertestapi.tar versacall@192.168.100.231:dockertestapi.tar
+sshpass -p $pass scp docker-compose.yml versacall@192.168.100.231:docker-compose.yml
+
 "rm"
 rm react-app.tar
 rm dockertestapi.tar
+
 "docker load"
-ssh versacall@192.168.100.231 "docker load -i react-app.tar && docker load -i dockertestapi.tar"
+sshpass -p $pass ssh versacall@192.168.100.231 "docker load -i react-app.tar && docker load -i dockertestapi.tar"
 pause
