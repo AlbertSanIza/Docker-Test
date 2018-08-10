9830763fb6aba1a32f248161496439006305ee42
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-10 15:04:13 -0700

    auto indent

--- react-lib/example/src/App.js
+++ react-lib/example/src/App.js
@@ -3,15 +3,15 @@ import { Example } from 'react-lib'
 import { Heroes, Translation } from './react-lib-redux/'
 
 export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <Example text='Modern React component module'/>
-        <hr/>
-        <Heroes/>
-        <hr/>
-        <Translation/>
-      </div>
-    )
-  }
+    render () {
+        return (
+            <div>
+                <Example text='Modern React component module'/>
+                <hr/>
+                <Heroes/>
+                <hr/>
+                <Translation/>
+            </div>
+        )
+    }
 }
