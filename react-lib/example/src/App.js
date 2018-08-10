7b397de885880f341a7611e6d921f332209ea5c6
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-10 13:46:57 -0700

    show new component

--- react-lib/example/src/App.js
+++ react-lib/example/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import { Example } from 'react-lib'
-import Heroes from './react-lib-redux'
+import { Heroes, Translation } from './react-lib-redux/'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <Example text='Modern React component module' />
+        <Example text='Modern React component module'/>
+        <hr/>
         <Heroes/>
+        <hr/>
+        <Translation/>
       </div>
     )
   }
